84bf7696c646cd03293bbb4fb6214bf6c076e720
ejthomas <ed.j.thomas@protonmail.com>
2020-05-23 16:26:44 +0100

    Initial commit with readme and core script

---
+++ README.md
@@ -0,0 +1,9 @@
+# URL Alarm Clock
+
+A command-line application to load a user's chosen URL at a time specified.
+
+## Installation
+Not yet available.
+
+## Usage
+Not yet available.
---
+++ urlalarm.py
@@ -0,0 +1,13 @@
+# URL Alarm: Load a web page by URL at a specified time
+
+import time
+import webbrowser
+import argparse
+
+def get_delay(target_time):
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
